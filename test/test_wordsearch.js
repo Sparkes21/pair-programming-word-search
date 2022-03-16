9ff202e24d37c227a0ce406d4291a59db51b8d44
Josh Sparkes <jshsparx@gmail.com>
2022-03-16 15:31:01 -0600

    pairprogamming word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,23 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return false if letter is an empty array", function() {
+    const result = wordSearch([], 'SEINFELD');
+    assert.isFalse(result);
+  });
+  it("should return true the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'I', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'E', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'E', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'R', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'D', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'O', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'G', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WIENERDOG');
+    assert.isFalse(result);
+  });
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    console.log(horizontalJoin);
+    for (const l of horizontalJoin) {
+        
+      if (l.includes(word)) {
+        return true;
+      }
     }
-}
-
-module.exports = wordSearch
+    for (const l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  
+  const transpose = function(matrix) {
+    // Put your solution here
+    let newMatrix = [];
+    if (matrix.length === 1) {
+      for (let x = 0; x < matrix[0].length; x++) {
+        newMatrix.push([]);
+        newMatrix[x].push(matrix[0][x]);
+      }
+      return newMatrix;
+    }
+    
+    for (let y = 0; y < matrix.length; y++) {
+      newMatrix.push([]);
+    
+      for (let x = 0; x < matrix[y].length; x++) {
+        newMatrix[y].push(matrix[y][x]);
+      }
+    
+    }
+    
+    return newMatrix;
+  };
+    
+  
+  module.exports = wordSearch;
+  
